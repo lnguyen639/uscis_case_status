1ec51860a11a995a84412f74440981285d52f0f3
Linh <lnguyen639@gmail.com>
2022-04-13 22:07:14 -0700

    Add download bill capability and print bill amount

--- smud.py
+++ smud.py
@@ -9,13 +9,11 @@
 #   Then just use session.get(someurl, ...) to retrieve pages etc
 
 import requests
-import argparse
 from bs4 import BeautifulSoup
 from secret import username, passwd
 
-URL = 'https://myaccount.smud.org/?Length=0'
-URL = 'https://myaccount.smud.org/?ack=true/'
-URL = 'https://myaccount.smud.org/signin/index?ReturnUrl=%2fmanage%2fresidential%2fdashboard'
+SMUD_URL = 'https://myaccount.smud.org'
+SMUD_BILL_URL = f'{SMUD_URL}/manage/digitalbill'
 payload = {
     'UserID': username,
     'Password': passwd,
@@ -26,11 +24,29 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'
 }
 with requests.Session() as s:
-    req = s.get(URL, headers=headers)
-    page = BeautifulSoup(req.text)
+    # GET from smud to get the session token
+    req = s.get(SMUD_URL, headers=headers)
+    page = BeautifulSoup(req.text, 'lxml')
     token = page.find('input', {'name': '__RequestVerificationToken'}).get('value')
+
+    # Prepare payload/headers then do a POST to login
     payload['__RequestVerificationToken'] = token
     headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    req1 = s.post(SMUD_URL, data=payload, headers=headers)
 
-    req1 = s.post(URL, data=payload, headers=headers)
-    print(req1.content)
+    # Get download link for the bill
+    req2 = s.get(SMUD_BILL_URL, headers=headers)
+    page2 = BeautifulSoup(req2.text, 'lxml')
+    bill_query = page2.find('span', {'class': 'print-link-container async-load-or-redirect'}).get('data-load-url')
+
+    # Download bill and save to local
+    req3 = s.get(f'{SMUD_URL}{bill_query}')
+    page3 = BeautifulSoup(req3.text, 'lxml')
+    bill_download = page3.find('a', {'class':"print-link d-block gtm-dig-bill-link-print"}).get('href')
+    req4 = s.get(bill_download)
+    with open('smud.pdf', 'wb') as fh:
+        fh.write(req4.content)
+
+    # Grab bill amount to print
+    bill_amt = page2.find('div', {'class': 'summary-amount'}).findChild('span', {'class': 'cost-item'}).contents[0]
+    print(bill_amt)
